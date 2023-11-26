c0830394688c36231cf267ea11566b52e8cd47e5
hb_w <116944008+qiuqiuyaaa@users.noreply.github.com>
2023-11-26 20:21:48 +0800

    Update main1.m

--- main1.m
+++ main1.m
@@ -1,33 +1,31 @@
 
 clear all 
 clc
-N=20; %种群数量
-M_Iter=100; %设定最大迭代次数
-LB = 0;%上边界
-UB = 2;%下边界
-Dim = 50;%分的段数
-F1 = @(x) x^2 ; %定义要测试的积分函数
-F2 = @(x) x^4;  %定义要测试的积分函数
-F3 = @(x) sqrt(1 + x^2);  %定义要测试的积分函数
+N=20; 
+M_Iter=100; 
+LB = 0;
+UB = 2;
+Dim = 50;
+F1 = @(x) x^2 ; 
+F2 = @(x) x^4;  
+F3 = @(x) sqrt(1 + x^2); 
 F4 = @(x) 1/(x + 1);
 F5 = @(x) sin(x);
 F6 = @(x) exp(x);
 F7 = @(x) x(1)^2+x(2)^2;
-%选择积分函数
+
 f = F7;
-F_obj = @(x) fun(x,LB,UB);%定义适应度函数
-[Best_pos,Best_F,Conv_curve]=HAOA(N,M_Iter,LB,UB,Dim,F_obj); %开始优化
-%计算最终的积分
+F_obj = @(x) fun(x,LB,UB);
+[Best_pos,Best_F,Conv_curve]=HAOA(N,M_Iter,LB,UB,Dim,F_obj);
+
 J1 = CalulateInf(Best_pos,f,LB,UB);
-disp(['最终积分结果J1: ',num2str(J1)]);
+disp(['J1: ',num2str(J1)]);
 figure
 semilogy(Conv_curve,'Color','r','LineWidth',2);
-title('收敛曲线')
-xlabel('迭代次数');
-ylabel('适应度值');
+
 axis tight
 grid on
 box on
 legend('HAOA')
-% disp (Conv_curve);
+disp (Conv_curve);
 
